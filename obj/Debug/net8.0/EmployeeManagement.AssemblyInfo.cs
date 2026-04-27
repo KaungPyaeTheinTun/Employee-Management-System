7c20f4ca5e829101ceb38e5da535c4772ecbed97
Kaung Pyae Thein Tun <kaungpyaetheintun@Kaungs-MacBook-Air.local>
2026-04-27 21:34:02 +0630

    Update DataTable Search.

--- obj/Debug/net8.0/EmployeeManagement.AssemblyInfo.cs
+++ obj/Debug/net8.0/EmployeeManagement.AssemblyInfo.cs
@@ -10,10 +10,11 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e8140206a3d9d5a8304baf2a8dd3b25d6392521")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a74bf11b4d5772720c01cb457d81b82c4b0b3d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
